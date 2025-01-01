5b30445df7930ee0baf0354859b3a4f622eed45d
dani-77 <daniazevedo77@posteo.net>
2025-01-01 01:24:27 +0000

    rules

--- skel.sh
+++ skel.sh
@@ -4,3 +4,4 @@ cp -r greeny_dark tmp.*/image/rootfs/usr/share/slim/themes/
 cp -r skel tmp.*/image/rootfs/etc/
 cp slim.conf tmp.*/image/rootfs/etc/
 cp rofi-power-menu tmp.*/image/rootfs/usr/bin/
+cp 50-udisks.rules tmp.*/image/rootfs/usr/share/polkit-1/rules.d/
