c4c15c651836d0f36941a2b7eb53f310099cc24e
dani-77 <daniazevedo77@posteo.net>
2025-01-07 22:20:20 +0000

    ncspot

--- skel.sh
+++ skel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -r greeny_dark tmp.*/image/rootfs/usr/share/slim/themes/
-cp -r skel tmp.*/image/rootfs/etc/
+cp -r greeny_dark/ tmp.*/image/rootfs/usr/share/slim/themes/
+cp -r skel/ tmp.*/image/rootfs/etc/
 cp slim.conf tmp.*/image/rootfs/etc/
 cp rofi-power-menu tmp.*/image/rootfs/usr/bin/
 cp 50-udisks.rules tmp.*/image/rootfs/usr/share/polkit-1/rules.d/
