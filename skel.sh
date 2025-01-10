33475444649bb750baf4458fb640091229adef8c
dani-77 <daniazevedo77@posteo.net>
2025-01-10 10:04:35 +0000

    splash.png

--- skel.sh
+++ skel.sh
@@ -5,3 +5,4 @@ cp -r skel/ tmp.*/image/rootfs/etc/
 cp slim.conf tmp.*/image/rootfs/etc/
 cp rofi-power-menu tmp.*/image/rootfs/usr/bin/
 cp 50-udisks.rules tmp.*/image/rootfs/usr/share/polkit-1/rules.d/
+cp data/splash.png tmp.*/image/rootfs/usr/share/void-artwork/
