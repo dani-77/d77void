2615f252539d93e25e8af8ae1a1009428ac43b77
dani-77 <daniazevedo77@posteo.net>
2025-01-14 19:39:01 +0000

    openbox

--- fluxbox/skel.sh
+++ fluxbox/skel.sh
@@ -5,4 +5,4 @@ cp -r skel/ mklive-build.*/image/rootfs/etc/
 cp slim.conf mklive-build.*/image/rootfs/etc/
 cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
 cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
-cp data/splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
+cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
