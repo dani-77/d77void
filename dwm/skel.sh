c05583e81e9fb5f12252506fdc8524a3ae859048
dani-77 <daniazevedo77@posteo.net>
2025-01-16 15:21:48 +0000

    dwm

--- dwm/skel.sh
+++ dwm/skel.sh
@@ -5,10 +5,9 @@ cp -r skel/ mklive-build.*/image/rootfs/etc/
 cp slim.conf mklive-build.*/image/rootfs/etc/
 cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
 cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
-cp dmen* mklive-build.*/image/rootfs/usr/bin/
 cp dwm/dwm mklive-build.*/image/rootfs/usr/bin/
 cp slock mklive-build.*/image/rootfs/usr/bin/
 cp slstatus mklive-build.*/image/rootfs/usr/bin/
 cp st mklive-build.*/image/rootfs/usr/bin/
-cp stest mklive-build.*/image/rootfs/usr/bin/
+cp st-* mklive-build.*/image/rootfs/usr/share/terminfo/s/
 cp power_menu mklive-build.*/image/rootfs/usr/bin/
