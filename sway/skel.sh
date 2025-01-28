1932001ceab68ef4ff24954e76d0396c4fa65a0a
dani-77 <daniazevedo77@posteo.net>
2025-01-28 20:43:08 +0000

    sddm

--- sway/skel.sh
+++ sway/skel.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp -r greeny_dark/ mklive-build.*/image/rootfs/usr/share/slim/themes/
 cp -r skel/ mklive-build.*/image/rootfs/etc/
 cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
 cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
