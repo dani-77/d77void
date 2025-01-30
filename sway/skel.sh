5fafd4c298670e8470155107a8835685b8505a02
dani-77 <daniazevedo77@posteo.net>
2025-01-30 10:57:59 +0000

    gaps_faces

--- sway/skel.sh
+++ sway/skel.sh
@@ -2,7 +2,8 @@
 
 cp -r skel/ mklive-build.*/image/rootfs/etc/
 cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
+cp anon.face.icon mklive-build.*/image/rootfs/usr/share/sddm/faces
+cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
 cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
 cp st mklive-build.*/image/rootfs/usr/bin/
-cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
 cp st-* mklive-build.*/image/rootfs/usr/share/terminfo/s/
