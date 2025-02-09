81d1139c40f5830addcc357072ba80660d9b6f40
dani-77 <daniazevedo77@posteo.net>
2025-02-09 15:29:50 +0000

    papirus

--- hyprland/skel.sh
+++ hyprland/skel.sh
@@ -5,6 +5,5 @@ cp -r slice/ mklive-build.*/image/rootfs/usr/share/sddm/themes/
 cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
 cp anon.face.icon mklive-build.*/image/rootfs/usr/share/sddm/faces
 cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
-cp sddm.conf mklive-build.*/image/rootfs/etc/
 cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
-cp hyprland-void.conf mklive-build.*/image/rootfs/etc/xbps.d/
+cp wswap-way mklive-build.*/image/rootfs/usr/bin/
