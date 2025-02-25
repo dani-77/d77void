c2914788fe496d4299e67c04b4148afd2a395850
dani-77 <daniazevedo77@posteo.net>
2025-02-25 16:36:43 +0000

    rebrand

--- fluxbox/skel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -r greeny_dark/ mklive-build.*/image/rootfs/usr/share/slim/themes/
-cp -r skel/ mklive-build.*/image/rootfs/etc/
-cp slim.conf mklive-build.*/image/rootfs/etc/
-cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
-cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
-cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
-cp wswap-X mklive-build.*/image/rootfs/usr/bin/
--- labwc/skel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -r skel/ mklive-build.*/image/rootfs/etc/
-cp -r slice/ mklive-build.*/image/rootfs/usr/share/sddm/themes/
-cp 50-udisks.rules mklive-build.*/image/rootfs/usr/share/polkit-1/rules.d/
-cp anon.face.icon mklive-build.*/image/rootfs/usr/share/sddm/faces
-cp rofi-power-menu mklive-build.*/image/rootfs/usr/bin/
-cp splash.png mklive-build.*/image/rootfs/usr/share/void-artwork/
-cp wswap-way mklive-build.*/image/rootfs/usr/bin/
