5e4e2f89f7a6531f5462dab17234cf8f7d6c8292
dani-77 <daniazevedo77@posteo.net>
2025-03-09 22:08:08 +0000

    qtile

--- qtile/skel/.config/qtile/autostart.sh
+++ qtile/skel/.config/qtile/autostart.sh
@@ -2,8 +2,3 @@
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark" &
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &
 wlsunset -t 3500 -T 5700 -l 41.6 -L -8.62 -g 0.8 &
-synclient TapButton1=1 &
-synclient TapButton2=3 &
-synclient TapButton3=2 &
-xcompmgr -c -f -n &
-redshift -l 41.6:-8.62 &
