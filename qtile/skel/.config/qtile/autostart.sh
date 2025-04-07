af9f5019b4664d32a34bf32cc8c135d210082309
Daniel Azevedo <daniazevedo77@posteo.net>
2025-04-07 16:17:57 +0100

    qtile_dark

--- qtile/skel/.config/qtile/autostart.sh
+++ qtile/skel/.config/qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export QT_QPA_PLATFORM=wayland
 gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark" &
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' &
 wlsunset -t 3500 -T 5700 -l 41.6 -L -8.62 -g 0.8 &
