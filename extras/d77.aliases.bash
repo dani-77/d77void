6f11adeb40430858e2b74900a702e828ae531453
Daniel Azevedo <daniazevedo77@posteo.net>
2025-05-04 10:54:37 +0100

    d77aliases

---
+++ extras/d77.aliases.bash
@@ -0,0 +1,20 @@
+# shellcheck shell=bash
+#
+# -binaryanomaly
+
+cite 'about-alias'
+about-alias 'XBPS and xbps-src aliases for d77void, Void and Void derivative distros.'
+
+# set xbps aliases
+function _set_pkg_aliases() {
+	if _command_exists xbps-install; then
+		alias pkgsearch='xbps-query -Rs'
+		alias pkginst='sudo xbps-install -S'
+		alias pkgupg='sudo xbps-install -Syu'
+		alias pkgrm='sudo xbps-remove -R'
+
+		alias srcbuild='./xbps-src pkg'
+	fi
+}
+
+_set_pkg_aliases
