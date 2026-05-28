6b66c682a2393c0aa9221d2cdb79f388215d5d27
Daniel Azevedo <daniazevedo77@posteo.net>
2026-05-28 11:58:52 +0100

    awesome-appmenu no-icons

--- somewm/skel/.config/awesome/appmenu.lua
+++ somewm/skel/.config/awesome/appmenu.lua
@@ -1,49 +1,49 @@
 local appmenu = {}
 
 appmenu.Accessories = {
-    { 'Kvantum Manager', 'kvantummanager', '/usr/share/icons/hicolor/scalable/apps/kvantum.svg' },
-    { 'Mousepad', 'mousepad', '/usr/share/icons/hicolor/96x96/apps/org.xfce.mousepad.png' },
-    { 'Xarchiver', 'xarchiver', '/usr/share/icons/hicolor/scalable/apps/xarchiver.svg' },
+    { 'Kvantum Manager', 'kvantummanager' },
+    { 'Mousepad', 'mousepad' },
+    { 'Xarchiver', 'xarchiver' },
 }
 
 appmenu.Internet = {
     { 'Brave Web Browser', '/usr/bin/brave-browser-stable' },
-    { 'Geary', 'geary', '/usr/share/icons/hicolor/scalable/apps/org.gnome.Geary.svg' },
+    { 'Geary', 'geary' },
     { 'Transmission (Qt)', 'transmission-qt' },
-    { 'uGet', 'env GDK_BACKEND=x11 uget-gtk', '/usr/share/icons/hicolor/96x96/apps/uget-icon.png' },
+    { 'uGet', 'env GDK_BACKEND=x11 uget-gtk' },
 }
 
 appmenu.Office = {
-    { 'Zathura', 'zathura', '/usr/share/icons/hicolor/scalable/apps/org.pwmt.zathura.svg' },
+    { 'Zathura', 'zathura' },
 }
 
 appmenu.MultiMedia = {
-    { 'Echomixer', 'echomixer', '/usr/share/icons/hicolor/48x48/apps/echomixer.png' },
-    { 'Envy24 Control', 'envy24control', '/usr/share/icons/hicolor/48x48/apps/envy24control.png' },
+    { 'Echomixer', 'echomixer' },
+    { 'Envy24 Control', 'envy24control' },
     { 'HDAJackRetask', 'hdajackretask' },
-    { 'HDSPConf', 'hdspconf', '/usr/share/icons/hicolor/48x48/apps/hdspconf.png' },
-    { 'HDSPMixer', 'hdspmixer', '/usr/share/icons/hicolor/48x48/apps/hdspmixer.png' },
-    { 'Hwmixvolume', 'hwmixvolume', '/usr/share/icons/hicolor/128x128/apps/hwmixvolume.png' },
-    { 'mpv Media Player', 'mpv --player-operation-mode=pseudo-gui --', '/usr/share/icons/hicolor/scalable/apps/mpv.svg' },
+    { 'HDSPConf', 'hdspconf' },
+    { 'HDSPMixer', 'hdspmixer' },
+    { 'Hwmixvolume', 'hwmixvolume' },
+    { 'mpv Media Player', 'mpv --player-operation-mode=pseudo-gui --' },
 }
 
 appmenu.Settings = {
     { 'Desktop Preferences', 'pcmanfm --desktop-pref' },
     { 'GTK Settings', 'nwg-look' },
-    { 'Kvantum Manager', 'kvantummanager', '/usr/share/icons/hicolor/scalable/apps/kvantum.svg' },
-    { 'Manage Printing', 'xdg-open http://localhost:631/', '/usr/share/icons/hicolor/32x32/apps/cups.png' },
+    { 'Kvantum Manager', 'kvantummanager' },
+    { 'Manage Printing', 'xdg-open http://localhost:631/' },
     { 'Print Settings', 'system-config-printer' },
     { 'Qt5 Settings', 'qt5ct' },
     { 'Qt6 Settings', 'qt6ct' },
 }
 
 appmenu.System = {
-    { 'Alacritty', 'alacritty', '/usr/share/icons/hicolor/scalable/apps/Alacritty.svg' },
-    { 'Htop', 'xterm -e htop', '/usr/share/icons/hicolor/scalable/apps/htop.svg' },
-    { 'Install System', 'sudo -E calamares', '/usr/share/icons/hicolor/scalable/apps/calamares.svg' },
-    { 'Manage Printing', 'xdg-open http://localhost:631/', '/usr/share/icons/hicolor/32x32/apps/cups.png' },
-    { 'OctoXBPS', '/usr/bin/octoxbps', '/usr/share/icons/hicolor/48x48/apps/octopi.png' },
-    { 'OctoXBPS Notifier', '/usr/bin/octoxbps-notifier', '/usr/share/icons/hicolor/48x48/apps/octopi.png' },
+    { 'Alacritty', 'alacritty' },
+    { 'Htop', 'xterm -e htop' },
+    { 'Install System', 'sudo -E calamares' },
+    { 'Manage Printing', 'xdg-open http://localhost:631/' },
+    { 'OctoXBPS', '/usr/bin/octoxbps' },
+    { 'OctoXBPS Notifier', '/usr/bin/octoxbps-notifier' },
     { 'PCMan File Manager', 'pcmanfm' },
     { 'Print Settings', 'system-config-printer' },
     { 'd77-welcome', 'xterm -e d77-welcome' },
