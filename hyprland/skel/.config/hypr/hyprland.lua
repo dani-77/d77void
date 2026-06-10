bbe45d9f3da2e5a433c2477e514164424585c716
Daniel Azevedo <daniazevedo77@posteo.net>
2026-06-11 00:44:42 +0100

    hyprpaper as wallpaper manager hyprland variant

--- hyprland/skel/.config/hypr/hyprland.lua
+++ hyprland/skel/.config/hypr/hyprland.lua
@@ -50,7 +50,7 @@ hl.on("hyprland.start", function ()
 	hl.exec_cmd("quickshell")
 	hl.exec_cmd("hyprsunset")
 	hl.exec_cmd("/usr/libexec/hyprpolkitagent")
-	hl.exec_cmd("~/.azotebg-hyprland")
+	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("hypridle")
 end)
 
