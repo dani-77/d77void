497347400ded687aef8cec6c6fe19572b7eb8470
Daniel Azevedo <daniazevedo77@posteo.net>
2026-07-08 14:49:56 +0100

    auto.sh 2nd button

--- herbstluftwm/skel/.auto.sh
+++ herbstluftwm/skel/.auto.sh
@@ -1,1 +1,5 @@
-yad --text-info --filename="$HOME/README.md" --width=0600 --height=450 --title="README.md"  --button="close:0"
+yad --text-info --filename="$HOME/README.md" --width=0600 --height=450 --title="README.md" --button="Don't show this again:1" --button="close:0"
+
+if [ $? -eq 1 ]; then
+	sed -i '/\.auto\.sh/d' "$HOME/.config/herbstluftwm/autostart"
+fi
