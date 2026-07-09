e167d05609abbf15edcc23c60dba7fb9a1e02675
Daniel Azevedo <daniazevedo77@posteo.net>
2026-07-09 10:54:10 +0100

    new d77-welcome and autostart

--- scroll/skel/.auto.sh
+++ scroll/skel/.auto.sh
@@ -1,5 +1,2 @@
-yad --text-info --filename="$HOME/README.md" --width=0600 --height=450 --title="README.md" --button="Don't show this again:1" --button="close:0"
-
-if [ $? -eq 1 ]; then
-	rm -f -- "$HOME/.auto.sh"
-fi
+#!/bin/sh
+exec d77-welcome --open
